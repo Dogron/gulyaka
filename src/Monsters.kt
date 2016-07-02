7f6ef0550cc8ada69473218f203d4256e395c40e
Dogron <fedor.ryzhenkov@gmail.com>
2016-07-02 21:08:58 +0300

    Класс Monster и поведение damage

--- src/Monsters.kt
+++ src/Monsters.kt
@@ -3,27 +3,40 @@
  */
 package gulyaka
 
-class Monster(val name: String, val initialHP: Int, val maxDamage: Int)
+class MonsterType(val name: String, val initialHP: Int, val maxDamage: Int) {
+    fun createMonster() : Monster {
+        return Monster(name, initialHP, maxDamage)
+    }
+}
 
-val monsters = arrayOf(
-        Monster("Гоблин", 5, 4),
-        Monster("Разбойник", 3, 10)
+class Monster(val name: String, var HP: Int, val maxDamage: Int) {
+    fun damage(points: Int) {
+        HP -= points
+        println("Вы ударили на $points HP")
+        if (HP < 0) {
+            HP = 0
+        }
+    }
+}
+
+val monsterTypes = arrayOf(
+        MonsterType("Гоблин", 5, 4),
+        MonsterType("Разбойник", 3, 10)
 )
 
 fun Monster() {
-    val monster = monsters[random.nextInt(monsters.size)]
-    var hpMonster = monster.initialHP
+    val monsterType = monsterTypes[random.nextInt(monsterTypes.size)]
+    val monster = monsterType.createMonster()
+
     println("Вы встретили ${monster.name}.")
-    println("У ${monster.name} $hpMonster HP")
+    println("У ${monster.name} ${monster.HP} HP")
     while (true) {
-        val playerAttack = random.nextInt(4) + 1
-        println("Вы ударили на $playerAttack HP.")
-        hpMonster -= playerAttack
-        if (hpMonster <= 0) {
+        monster.damage(random.nextInt(4) + 1)
+        if (monster.HP == 0) {
             println("${monster.name} побеждён.")
             return
         } else
-            println("у ${monster.name} осталось $hpMonster HP")
+            println("у ${monster.name} осталось ${monster.HP} HP")
         damage(random.nextInt(monster.maxDamage) + 1)
         if (HP <= 0) {
             break
